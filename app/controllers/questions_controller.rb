14d6b1fe22ffe244dc6efde8dd41a964d233efe4
ViktoriaB2018 <bvo2008@mail.ru>
2019-02-24 15:27:09 +0200

    Fixed bag - deleted author_id from permit in Questions_controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -44,9 +44,9 @@ class QuestionsController < ApplicationController
   def question_params
     if current_user.present? &&
        params[:question][:user_id].to_i == current_user.id
-      params.require(:question).permit(:user_id, :author_id, :text, :answer)
+      params.require(:question).permit(:user_id, :text, :answer)
     else
-      params.require(:question).permit(:user_id, :author_id, :text)
+      params.require(:question).permit(:user_id, :text)
     end
   end
 end
